35f58cfef6c02baae6e6a722a6abe8e363ebae89
Kazi1233 <78030886+Kazi1233@users.noreply.github.com>
2021-07-04 20:04:11 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,54 @@
+class Paper {
+constructor(x,y,r){
+      var op={
+      isStatic:false,
+      restitution:0.3,
+      friction:0,
+      density:1.2
+
+
+
+      }
+this.x=x;
+this.y=y;
+this.r=r;
+
+this.image=loadImage("paper.png")
+this.body=Matter.Bodies.circle(x,y,r,op)
+World.add(world,this.body)
+
+
+}
+display (){
+
+push ()
+
+
+translate (this.body.position.x,this.body.position.y)
+imageMode (CENTER);
+
+
+image (this.image,0,0,this.r,this.r)
+
+pop ()
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
